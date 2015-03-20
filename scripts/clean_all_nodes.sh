9aa13c38fd64ceb57918b125f0b19d22b553c14f
Ubuntu <ubuntu@ip-172-31-7-21.ec2.internal>
2015-03-20 00:22:33 +0000

    Remove unuseful.

--- scripts/clean_all_nodes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo cleaning nodes
-
-Nodes=$(cat scripts/allnodes)
-./scripts/stopNodes.sh "$Nodes"
--- scripts/read_ip.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ip in $(cat ip_list)
-do
-	ssh -i key ubuntu@$ip "ifconfig | grep Bcast | awk '{print $2}' | awk -F":" '{print $2}'"
-done
