c96924e48f44f847ee5aef060d2ec7374cbefdd4
Ubuntu <ubuntu@benchmark.c.electric-nomad-107617.internal>
2015-10-30 02:48:40 +0000

    Add more script

--- scripts/checkThroughput.sh
+++ scripts/checkThroughput.sh
@@ -12,7 +12,7 @@
 ./scripts/cleanAllNodes.sh
 
 ExistingNodes=`head -2 scripts/allnodes`
-NodesToAdd=`tail -2 scripts/allnodes`
+NodesToAdd=`tail -1 scripts/allnodes`
 FirstNode=`head -1 scripts/allnodes`
 echo "Existing nodes are " "$ExistingNodes" ", nodes to add are " "$NodesToAdd"
 AllNodes=$ExistingNodes" "$NodesToAdd
@@ -26,27 +26,40 @@ do
 	./scripts/startNodes.sh "$ExistingNodes"
 	echo "Rebalance limit: "$Limit", Write Ratio: "$WRatio", Operation target: "$Target"op/s."
 
-	./scripts/load.sh "$ExistingNodes" 150000
-	./scripts/checkPortStat.sh "$NodesToAdd" 150 $Folder &
+	./scripts/load.sh "$ExistingNodes" 180000
+	#Sleep for balancing
 	sleep 60
+	./scripts/command_to_all.sh "$NodesToAdd" "sudo iptables -A OUTPUT -p tcp --dport 9042"
+	./scripts/checkPortStat.sh "$NodesToAdd" 180 $Folder &
+	./scripts/runWorkload.sh "$AllNodes" $Folder 0.1 0 200 &
+	sleep 30
 	./scripts/fetchNetworkUsg.sh $Folder  start
 
+	T=`date +'%Y-%m-%d-%H:%M:%S'`
+	TInSec=`date +%s`
+	for N in ${NodesToAdd}
+	do
+	    echo "Rebalance started" $T  >> $Folder/$N
+	done
 	./scripts/addNode.sh "$NodesToAdd"
-	./scripts/command_to_all.sh "$NodesToAdd" "sudo iptables -A OUTPUT -p tcp --dport 9042"
 	./scripts/rebalance/rebalance_started.sh $FirstNode
 
 	Time=`date +'%Y-%m-%d-%H:%M:%S'`
 	TimeInSec=`date +%s`
 	echo "Started at "$Time
-	./scripts/runWorkload.sh "$AllNodes" $Folder 0.1 0 80 &
 	./scripts/rebalance/rebalance_finished.sh $FirstNode
+	T2=`date +'%Y-%m-%d-%H:%M:%S'`
+	for N in ${NodesToAdd}
+	do
+	    echo "Rebalance finished" $T2 >> $Folder/$N
+	done
 
 	##Output rebalance time and latency
 	NewTime=`date +'%Y-%m-%d-%H:%M:%S'`
 	NewTimeInSec=`date +%s`
 	echo "Finished data transfer at "$NewTime
 	Diff=$((NewTimeInSec-TimeInSec))
-	echo "Finished at":$NewTimeInSec", used"$Diff >> $Folder"/rebalance_time"
+	echo "Finished at":$NewTime", used"$Diff >> $Folder"/rebalance_time"
 
 	for job in `jobs -p`
         do
