b6b44ec25431d353c1670c4a19b62b3928c632a9
Ubuntu <ubuntu@ip-172-31-13-229.eu-west-1.compute.internal>
2015-11-16 23:30:36 +0000

    Add change

--- scripts/configMachine.sh
+++ scripts/configMachine.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 First=`head -1 ./scripts/allnodes`
 for node in `cat ./scripts/allnodes`
@@ -13,10 +14,10 @@ ReplaceHostName="sudo sed -i 's/.* -Djava.rmi.server.hostname=.*/JVM_OPTS=\x22$J
 #Increase read/write timeout
 IncreaseTimeout="sudo sed -i 's/write_request_timeout_in_ms: .*/write_request_timeout_in_ms: 100000/' $YAML && 
 		sudo sed -i 's/read_request_timeout_in_ms: .*/read_request_timeout_in_ms: 50000/' $YAML"
-ReduceDiskSync="sudo sed -i 's/commitlog_sync_period_in_ms: .*/commitlog_sync_period_in_ms: 2000/' $YAML" 
-ReducePendingCommit="sudo sed -i 's/.*commitlog_periodic_queue_size.*/commitlog_periodic_queue_size: 600/' $YAML" 
+#ReduceDiskSync="sudo sed -i 's/commitlog_sync_period_in_ms: .*/commitlog_sync_period_in_ms: 2000/' $YAML" 
+#ReducePendingCommit="sudo sed -i 's/.*commitlog_periodic_queue_size.*/commitlog_periodic_queue_size: 600/' $YAML" 
 ChangeHeapSize="sudo sed -i 's/#MAX_HEAP_SIZE.*/MAX_HEAP_SIZE=\x224G\x22/' $ENV" 
-ChangeNewHeap="sudo sed -i 's/#HEAP_NEWSIZE.*/HEAP_NEWSIZE=\x22400M\x22/' $ENV" 
+ChangeNewHeap="sudo sed -i 's/^HEAP_NEWSIZE.*/HEAP_NEWSIZE=\x22800M\x22/' $ENV" 
 #Change cluster name
 ChangeName="sudo sed -i 's/cluster_name: .*/cluster_name: \x27Rebel\x27/g' $YAML"
 #Replace seed
@@ -27,11 +28,11 @@ ReplaceListenAddr="sudo sed -i 's/listen_address:.*/listen_address: $IP/g' $YAML
 ./scripts/parallelCommand.sh $node "$ReplaceHost"
 ./scripts/parallelCommand.sh $node "$ReplaceHostName"
 ./scripts/parallelCommand.sh $node "$IncreaseTimeout"
-./scripts/parallelCommand.sh $node "$ReduceDiskSync"
 ./scripts/parallelCommand.sh $node "$ChangeName"
 ./scripts/parallelCommand.sh $node "$ChangeHeapSize"
 ./scripts/parallelCommand.sh $node "$ChangeNewHeap"
 ./scripts/parallelCommand.sh $node "$ReplaceSeed"
-./scripts/parallelCommand.sh $node "$ReducePendingCommit"
+#./scripts/parallelCommand.sh $node "$ReduceDiskSync"
+#./scripts/parallelCommand.sh $node "$ReducePendingCommit"
 ./scripts/parallelCommand.sh $node "$ReplaceListenAddr"
 done
