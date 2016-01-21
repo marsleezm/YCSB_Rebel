b56c0ec9122158e8a188f86b14bd0372da52727b
Ubuntu <ubuntu@ip-172-31-21-244.us-west-1.compute.internal>
2016-01-21 18:00:02 +0000

    Remove also in var

--- scripts/stopAndRemove.sh
+++ scripts/stopAndRemove.sh
@@ -12,7 +12,9 @@ FirstNode=${FirstNode[0]}
 Folder=`pwd`
 echo "Stopping all nodes:"$Nodes 
 stopC="sudo service cassandra stop"
+rmVarData="sudo rm -rf /var/lib/cassandra/data /var/lib/cassandra/commitlog /var/lib/cassandra/saved_caches"
 rmData="sudo rm -rf /mnt/cassandra_data/data /mnt/cassandra_data/commitlog /mnt/cassandra_data/saved_caches"
 $Folder/scripts/parallelCommand.sh "$Nodes" "$stopC"
 $Folder/scripts/parallelCommand.sh "$Nodes" "$rmData"
+$Folder/scripts/parallelCommand.sh "$Nodes" "$rmVarData"
 echo "Stopped nodes."
